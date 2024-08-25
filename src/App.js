f7ca87f8fd1e52ce3d22fd7739fbc5530a7606a4
wesam <wesam.a.s196@gmail.com>
2024-08-25 10:44:55 +0300

    add router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 function App() {
-
-
   return (
+    <Router>
     <div className="App">
       <Navbar />
         <div className="component">
-
-         <Home />
-        
- 
+        <Routes >
+         
+          <Route path='/' element={<Home />}/>
+         
+            
+         
+          
+         
+         </Routes >
         </div>
     </div>
+    </Router>
   );
 }
 
