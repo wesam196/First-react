db5de87e65aa762b5761b004bf832bbd7b0989dc
wesam <wesam.a.s196@gmail.com>
2024-09-03 02:50:25 +0300

    create blogDeatails

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,12 +1,42 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 
 function BlogDetails ()  {
     const {id} = useParams();
+    var {data:blog, isLoading, error} = useFetch('http://localhost:8000/blogs/' + id);
+
+   
+
+
     return (  
+        <div className="BlogDetails">
+
         
         <h1>Blog Details - {id}</h1>
+        
+        {error && <h1 >{error}</h1>}
+        {isLoading && <p>Loading...</p>}
 
+       
+             {blog &&
+                <div>
+                <h2>{blog.title}</h2>
+                <p>{blog.Auther}</p>
+                <p>{blog.body}</p>
+                <p>id = {blog.id}</p>
+                </div>
+            }
+
+
+
+
+
+
+        
+        
+        
+        </div>
 
 
     );
--- src/BlogLists.js
+++ src/BlogLists.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function BlogLists({blogs  })   {
     
     return (  
@@ -8,10 +10,12 @@ function BlogLists({blogs  })   {
             <div className="blog-preview" key={blog.id}>
              
                 <div>
+                    <Link to={`blog/${blog.id}`}>
                 <h2>{blog.title}</h2>
                 <p>{blog.Auther}</p>
                 <p>{blog.body}</p>
                 <p>id = {blog.id}</p>
+                </Link>
                 </div>
 
 
