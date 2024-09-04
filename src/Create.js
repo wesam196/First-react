139b3f6f2617e5bad69594ec9e6b2e257f469bab
wesam <wesam.a.s196@gmail.com>
2024-09-04 03:12:40 +0300

    add create form with css style

--- src/Create.js
+++ src/Create.js
@@ -1,8 +1,36 @@
+import { useState } from "react";
+
 const Create = () => {
+
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Wesam');
+
     return (
 
         <div className="create">
             <h2>Welcome To Create Page</h2>
+
+            <form>
+                <label>Blog Title</label>
+                <input type="text" required  value={title} onChange={(e)=> setTitle(e.target.value)} />
+
+                <label >Blog Body</label>
+                <textarea required  value={body} onChange={(e)=> setBody(e.target.value)} >
+                </textarea>
+
+                <label> Author</label>
+                <select value={author} onChange={(e)=> setAuthor(e.target.value)} >
+                    <option value="Wesam">Wesam</option>
+                    <option value="Ali">Ali</option>
+                    <option value="Omar">Omar</option>
+                </select>
+
+
+                <button>Add blog</button>
+        
+            </form>
+            
         </div>
 
       )
